f447f339e2f93ec7135a70a661619fb689013517
guilin 桂林 <guileen@gmail.com>
2011-05-08 18:49:24 +0800

    add demo.out.js

---
+++ demo.out.js
@@ -0,0 +1,40 @@
+var __cb=global.__cb || function(e){console.log(e)};// just for demo and test
+var mongo = require('mongodb')
+
+db = mongo.db('...');
+
+db.open( function(__err){if(__err)return __cb(__err);
+
+function getBooks(options,__cb){
+  db.collection('book', function(__err,book ){if(__err)return __cb(__err);
+  book.find( function(__err,cursor ){if(__err)return __cb(__err);
+  cursor.toArray( function(__err,books ){if(__err)return __cb(__err);
+  doSomething(books);
+  __cb(null,books);
+});});});}
+
+function multiLevels(__cb){
+  var data, books;
+  if(yes){
+    fs.readFile('filename', 'utf-8', function(__err,__data ){if(__err)return __cb(__err);data=__data ;
+    if(ok){
+      db.collection('book', function(__err,__books ){if(__err)return __cb(__err);books=__books ;
+      __cb(null,books);
+    });}
+  });}else{
+    fs.readFile('filename', 'utf-8', function(__err,__data ){if(__err)return __cb(__err);data=__data ;
+    if(ok){
+      db.collection('book', function(__err,__books ){if(__err)return __cb(__err);books=__books ;
+      __cb(null,books);
+    });}
+  });}
+  return books;//this is wrong, out of indent;
+}
+
+console.log('inside open');
+
+});//}
+
+console.log('outside open');
+console.log('start app');
+
